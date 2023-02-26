65cc39bf38d771eb0ac5e66d1d32b9c0f2024e9c
Katilina88 <caterinalograsso@gmail.com>
2023-02-26 18:32:17 +0100

    feat: aggiunto esercizio JSX-05

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+function App() {
+  const calc = (a, b) => {
+    return a + b; 
+
+  }
+ return (
+  <>
+  <h2>The result is {calc(2,2)}</h2>
+  </>
+ )
+}
+
+export default App;
