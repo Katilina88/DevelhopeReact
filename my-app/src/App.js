fd4e745ec4dd73d9387b504b161dbc39f04b7f84
Katilina88 <caterinalograsso@gmail.com>
2023-02-26 18:02:32 +0100

    feat: aggiunto esercizio Props-04

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Welcome} from './Welcome'; 
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Welcome name = {<strong>Caterina</strong>} age= {34} />
+
+      </div>
+    )
+  }
+}
+
+
+export default App;
