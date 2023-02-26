1bbe5b42c977a8550b9acddc16e2518af666366a
Katilina88 <caterinalograsso@gmail.com>
2023-02-26 12:03:47 +0100

    fead: aggiunto eserizio component 07

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Hello} from './Hello';
+
+
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <Hello/ >
+      </div>
+    )
+  }
+}
+
+export default App;
+
