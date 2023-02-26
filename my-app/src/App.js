bb1c12eeb069d6dd8c202bb8d2353d42b022510c
Katilina88 <caterinalograsso@gmail.com>
2023-02-26 18:13:16 +0100

    feat: aggiunto esercizio props-02

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Welcome} from './Welcome'; 
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Welcome/>
+
+      </div>
+    )
+  }
+}
+
+
+export default App;
