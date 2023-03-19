329a7c99066eef3014d1529eb2b1abdfceb3bfd2
Katilina88 <caterinalograsso@gmail.com>
2023-03-19 11:07:42 +0100

    aggiunto esercizio useState 01

--- my-app/src/components/ClickCounter.js
+++ my-app/src/components/ClickCounter.js
@@ -1,3 +1,5 @@
+import React, {useState} from "react"
+
 {/*import React from 'react'
 
 class ClickCounter extends React.Component {
@@ -25,4 +27,28 @@ class ClickCounter extends React.Component {
 }
 
 export default ClickCounter*/}
+
+
+
+function ClickCounter({initialValue = 0}) {
+    const[count, setCount] = useState(initialValue)
+
+    const clickHandler = () => {
+        setCount(count + 1);
+      };
+
+  return (
+    <div>
+        <h1>{count}</h1>
+       <button onClick={clickHandler}>Click me</button>
+      
+    </div>
+  )
+}
+
+export default ClickCounter
+
+
+
+
 
