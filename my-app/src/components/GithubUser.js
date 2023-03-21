2df7d68db49a2f4293cdddd1dd4b32638f107ee2
Katilina88 <caterinalograsso@gmail.com>
2023-03-21 10:44:27 +0100

    aggiunto esercizio custom-hook-04

--- my-app/src/components/GithubUser.js
+++ my-app/src/components/GithubUser.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import useGithubUser from './useGitHubUser';
 
+
+
 function GithubUser({ username }) {
-  const data = useGithubUser(username);
+  const [fetchUser, data, error, loading] = useGithubUser(username);
 
   return (
     <div>
-      {data && <h1>{data.name}</h1>}
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>{error.message}</h1>}
+      {data?.name && <h1>{data.name}</h1>}
     </div>
   );
 }
