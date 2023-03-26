67c637000ca878b703337538108415c16f934d42
Katilina88 <caterinalograsso@gmail.com>
2023-03-26 17:18:49 +0200

    esercizio fatto

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Welcome1 from "./components/Welcome1"
-
-
-
+import Welcome1 from "./components/Welcome1";
 
 function App() {
   return (
     <div>
-    
-                <Routes>
-                    <Route path='/' element={<Welcome1 name={'Caterina'}/>}/>
-                </Routes>
-            </div >
-   
-  )
+      <Routes>
+        <Route path="/" element={<Welcome1 name={"Caterina"} />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
