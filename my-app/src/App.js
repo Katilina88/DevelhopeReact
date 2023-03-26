983776d03d14afd2e3802e829e6f9fb3454189c5
Katilina88 <caterinalograsso@gmail.com>
2023-03-26 18:02:26 +0200

    aggiunto esercizio React-Router-02

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Welcome1 from "./components/Welcome1";
+import Counter1 from "./components/Counter1"; 
+import Container from "./components/Container"; 
 
 function App() {
   return (
-    <div>
+    <Container>
       <Routes>
         <Route path="/" element={<Welcome1 name={"Caterina"} />} />
+        <Route path="/counter1" element={<Counter1 /> } />
       </Routes>
-    </div>
+    </ Container>
   );
 }
 
---
+++ my-app/src/components/Counter1.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react"
+
+function ClickCounter({initialValue = 0}) {
+    const[count, setCount] = useState(initialValue)
+
+    const clickHandler = () => {
+        setCount(count + 1);
+      };
+
+  return (
+    <div>
+        <h1>{count}</h1>
+       <button onClick={clickHandler}>Click me</button>
+      
+    </div>
+  )
+}
+
+export default ClickCounter
