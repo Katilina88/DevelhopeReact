da35829cf532925ac3dd0532ab7b5a160d602ea3
Katilina88 <caterinalograsso@gmail.com>
2023-03-26 18:17:34 +0200

    aggiunto esercio React-Router-03

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Welcome1 from "./components/Welcome1";
 import Counter1 from "./components/Counter1"; 
 import Container from "./components/Container"; 
+import ShowGithubUser from "./components/ShowGithubUser";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome1 name={"Caterina"} />} />
         <Route path="/counter1" element={<Counter1 /> } />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
       </Routes>
     </ Container>
   );
--- my-app/src/components/GithubUser.js
+++ my-app/src/components/GithubUser.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import useGithubUser from './useGitHubUser';
+import useGithubUser from './useGithubUser';
 
 
 
